cd86926298e07b8a25c3d730306429e93e23158e
Lakshman Narayana <lakshman.yerasingu@gmail.com>
2025-09-19 08:53:38 +0530

    variables and problem statement

---
+++ problem1.py
@@ -0,0 +1,17 @@
+#step1 : vaiables initialization
+name = "lucky"
+age = 24
+Maths = 98.5
+sceience = 97.5
+coding = 99.5
+
+#calculating marks and average
+marks = Maths + sceience + coding
+average = marks / 3
+
+#adding to dictionary
+student = {"Name": name, "Age": age, "Marks": marks, "Average": average}
+
+#printing the dictionary
+print(student)
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+name = "lakshman"
+number = 10
+float_num = 22.55
+print(name)
+print(number)
+print(float_num)
+print(type(name))
+print(type(number))
+print(type(float_num))
